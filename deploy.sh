35b61d1e73fe0cbe41cd058fad475115d45efaa1
Daniel Villanueva <dvillaj@gmail.com>
2020-03-01 09:07:59 +0000

    Fix docker hub project name in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 echo "Using SHA of $GIT_SHA ..."
 
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker build -t dvillaj/multi-client:latest -t dvillaj/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t dvillaj/multi-server:latest -t dvillaj/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t dvillaj/multi-worker:latest -t dvillaj/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push dvillaj/multi-client:latest
+docker push dvillaj/multi-server:latest
+docker push dvillaj/multi-worker:latest
 
-docker push stephengrider/multi-client:$GIT_SHA
-docker push stephengrider/multi-server:$GIT_SHA
-docker push stephengrider/multi-worker:$GIT_SHA
+docker push dvillaj/multi-client:$GIT_SHA
+docker push dvillaj/multi-server:$GIT_SHA
+docker push dvillaj/multi-worker:$GIT_SHA
 
 echo "Applying changes in Kubernetes ..."
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$GIT_SHA
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$GIT_SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$GIT_SHA
+kubectl set image deployments/client-deployment client=dvillaj/multi-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=dvillaj/multi-server:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=dvillaj/multi-worker:$GIT_SHA
